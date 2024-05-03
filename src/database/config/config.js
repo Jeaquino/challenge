6394ef0f9509d05e16012ad7475e434ea80af5cb
Alexander <alexitorjlanus@gmail.com>
2024-05-03 15:47:07 -0300

    unas reformitas

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 module.exports = {
-    "development": {
-      "username": "root",
-      "password": 'Root2024',
-      "database": "library",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT
+      
   },
   test: {
     username: 'root',
