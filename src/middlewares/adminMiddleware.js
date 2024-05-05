6ab0eb1b788a1b0d36ccb93b08627bc6f267a5bf
Alexander <alexitorjlanus@gmail.com>
2024-05-05 06:55:27 -0300

    poniendo comits en el codigo para que se entienda un poco mas lo que hice

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,7 +1,11 @@
 function adminMiddleware(req, res, next) {
+    ////ponemos un estado llamado esAdmin, el cual empieza en false sea para usuario normal o para invitado
     res.locals.esAdmin=false
     if (req.session.usuarioLogueado!=undefined) {
+
+        //luego nos fijamos que si esta logueado el usuario logueado tenga el categoryid en 1 que es el de administrador, el 2 es normal
         if (req.session.usuarioLogueado.CategoryId == 1) {
+            ////en caso de ser 1 se habilita y se habilitaria las sesiones para administradores, como editar, borrar libros
             res.locals.esAdmin = true;
         } else {
             res.locals.esAdmin = false;
